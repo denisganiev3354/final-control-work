fada71af5f41b1ffb74b45a122f45e01c04ca63a
denisganiev3354 <denisganiev3354@gmail.com>
2023-05-03 10:57:34 +0400

    add the sort method 1 of the array and fill the array.

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -7,3 +7,33 @@
 
 string[] array1 = new string[] { "Hello", "ips", "1567", "Hi", "Kazan", "a" };
 string[] array2;
+
+array2 = SortArrayMoreThree(array1);
+
+string[] SortArrayMoreThree(string[] array)
+{
+	int countRightNumber = 0;
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i].Length <= 3)
+		{
+			countRightNumber++;
+		}
+	}
+
+	string[] newArray = new string[countRightNumber];
+
+	int tempCounter = 0;
+
+	for (int i = 0; i < array1.Length; i++)
+	{
+		if (array[i].Length <= 3)
+		{
+			newArray[tempCounter] = array[i];
+			tempCounter++;
+		}
+	}
+
+	return newArray;
+}
